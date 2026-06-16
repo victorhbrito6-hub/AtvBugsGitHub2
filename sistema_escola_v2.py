474ae12d273c63c60233fcbf32ec763caff30009
victor <victor.h.brito6@edu.senai.br>
2026-06-16 07:37:37 -0300

    Bug 2
    Alterei para somente (expoente) e tirei o "-1".

--- sistema_escola_v2.py
+++ sistema_escola_v2.py
@@ -27,11 +27,10 @@ def divisivel_por(numero, divisor):
     return numero % divisor == 0  
 
 # Função 2 - Calcula a potência sem usar o operador **
-# 🐛 BUG: range errado, faz uma multiplicação a menos
 def potencia(base, expoente):
     """Calcula base elevado a expoente usando multiplicação."""
     resultado = 1
-    for _ in range(expoente - 1):  # BUG: deveria ser range(expoente)
+    for _ in range(expoente):
         resultado *= base
     return resultado
 
