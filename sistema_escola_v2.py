3ec7a220cef55820e0660257a0b5f7da3d14fbc1
victor <victor.h.brito6@edu.senai.br>
2026-06-16 08:11:29 -0300

    bug 23
    Troquei os valores.

--- sistema_escola_v2.py
+++ sistema_escola_v2.py
@@ -235,7 +235,6 @@ def classificar_aluno(media, frequencia):
         return "Reprovado por nota"
 
 # Função 23 - Calcula desconto progressivo por quantidade
-# 🐛 BUG: os percentuais de desconto estão todos errados (trocados entre si)
 def calcular_desconto(preco, quantidade):
     """
     Aplica desconto por quantidade:
@@ -244,11 +243,11 @@ def calcular_desconto(preco, quantidade):
     - 10 ou mais    : 20% de desconto
     """
     if quantidade >= 10:
-        desconto = 0.05   # BUG: deveria ser 0.20
+        desconto = 0.20
     elif quantidade >= 5:
-        desconto = 0.20   # BUG: deveria ser 0.10
+        desconto = 0.10
     else:
-        desconto = 0.10   # BUG: deveria ser 0.05
+        desconto = 0.05
     return preco * (1 - desconto)
 
 # Função 24 - Fibonacci com recursão e memoização simples
